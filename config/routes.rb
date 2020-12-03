eae0bac0f1ed9c73bea89ba54037b5faa2e9f3e4
TakaiSaisei <takaisaisei@gmail.com>
2020-12-03 08:15:15 +0300

    Remove useless route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'logic#input'
-  get 'logic/input'
   get 'logic/view'
   get 'logic/dbview'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

176a4842ff9d95bd795079d10f273b4d9b36dbbc
TakaiSaisei <takaisaisei@gmail.com>
2020-12-06 05:47:50 +0300

    Well, uhh...

--- test/models/automorph_test.rb
+++ test/models/automorph_test.rb
@@ -4,16 +4,16 @@ require 'test_helper'
 
 class AutomorphTest < ActiveSupport::TestCase
   test 'add and search in db' do
-    instance = Automorph.new(input: 1, result: ActiveSupport::JSON.encode([[1, 1]]))
-    assert instance.save
+    result = Automorph.new(input: 1, result: ActiveSupport::JSON.encode([[1, 1]]))
+    assert result.save
     assert_equal ActiveSupport::JSON.encode([[1, 1]]), Automorph.find_by(input: 1).result
   end
 
   test 'check adding not unique value' do
-    instanse1 = Automorph.new(input: 1, result: ActiveSupport::JSON.encode([[1, 1]]))
-    instanse1.save
-    instance2 = Automorph.new(input: 1, result: ActiveSupport::JSON.encode([[1, 1]]))
-    instance2.validate
-    assert_equal instance2.errors[:input], ['has already been taken']
+    result_first = Automorph.new(input: 1, result: ActiveSupport::JSON.encode([[1, 1]]))
+    result_first.save
+    result_second = Automorph.new(input: 1, result: ActiveSupport::JSON.encode([[1, 1]]))
+    result_second.validate
+    assert_equal result_second.errors[:input], ['has already been taken']
   end
 end
